94fb7f84de9b1c3ba7cbc412609743396929fd77
ashleec13 <527achen@frhsd.com>
2026-04-20 10:39:40 -0400

    Still not working need to fix js

---
+++ script.js
@@ -0,0 +1,23 @@
+let tasks = []; // Array to hold tasks
+
+document.getElementById('addTaskBtn').addEventListener('click', function () {
+    let taskInput = document.getElementById('taskInput').value;
+    if (taskInput) {
+        task.push(taskInput); // Add task to array
+        document.getElementById('taskInput').value = ''; // Clear input field
+        displayTasks(); // Update task list display
+    }
+})
+
+
+function displayTasks() {
+    let taskList = document.getElementById('taskList');
+    taskList.innerHTML = ''; // Clear existing tasks
+    tasks.forEach((task, index) => {
+        let li = document.createElement('li')
+        li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center'
+        )
+        li.innerHTML = `${task} <button class="btn btn-success btn-sm" onclick="removeTask(${index})">✔</button>`;
+        taskList.appendChild(li);
+    });
+}
